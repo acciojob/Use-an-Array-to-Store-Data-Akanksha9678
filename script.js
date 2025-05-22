aaba876f92a31dd71ec64c2e530d958969b075f1
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-22 16:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let yourArray=[
+	{j:"apple",k:4,l:true},
+	{m:"grapes",n:5,o:false},
+	{g:"lichi",h:3,i:false},
+	{d:"mango",e:1,f:true},
+	{a:"banana",b:1,c:true}
+];
+let ul=document.querySelector("ul");
+for(let t of yourArray){
+	let li=document.createElement("li");
+	li.innerText=t;
+	ul.append(li);
+}
 
